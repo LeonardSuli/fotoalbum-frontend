243ac4b9f4beae5a206354c9acaa69760ba2694e
LeonardSuli <leonardsuli1991@gmail.com>
2024-06-15 17:19:47 +0200

    add pagination

--- src/App.vue
+++ src/App.vue
@@ -13,24 +13,29 @@ export default {
   },
 
   methods: {
+    goTo(url) {
+      console.log(url);
+      this.callApi(url);
+    },
+
+    // When i click the button search
     search() {
       const url =
-        this.base_api_url +
-        this.photos_endpoint +
-        `?search=${this.search_text}`;
+        this.base_api_url + this.photos_endpoint + `?search=${this.search_text}`;
 
       console.log(url);
 
       this.callApi(url);
     },
 
+    //
     callApi(url) {
       axios
         .get(url)
         .then((resp) => {
           console.log(resp);
 
-          this.photos = resp.data.results.data;
+          this.photos = resp.data.results;
           console.log(this.photos);
         })
         .catch((err) => {
@@ -53,6 +58,7 @@ export default {
   <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container py-2">
       <h1 class="display-5 fw-bold">PhotoAlbum</h1>
+
       <p class="col-md-8 fs-4">See our amazing album</p>
 
       <form @submit.prevent="search()">
@@ -63,18 +69,17 @@ export default {
             placeholder="Search..."
             v-model="search_text"
           />
-          <button class="btn btn-outline-secondary" type="submit">
-            Search
-          </button>
+
+          <button class="btn btn-outline-secondary" type="submit">Search</button>
         </div>
       </form>
     </div>
   </div>
 
-  <section class="photos" v-if="photos">
+  <section class="photos py-5" v-if="photos">
     <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-        <div class="col" v-for="photo in photos">
+        <div class="col" v-for="photo in photos.data">
           <div class="card">
             <div v-if="photo.upload_image">
               <img
@@ -98,6 +103,39 @@ export default {
           </div>
         </div>
       </div>
+
+      <nav aria-label="Page navigation" class="mt-4">
+        <ul class="pagination">
+          <li
+            class="page-item"
+            :class="{ disabled: !link.url, active: link.active }"
+            v-for="link in photos.links"
+          >
+            <button
+              class="page-link"
+              :href="link.url"
+              type="button"
+              @click="goTo(link.url)"
+            >
+              <span v-html="link.label"></span>
+            </button>
+          </li>
+
+          <!-- <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">1</a>
+          </li>
+
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+
+          <li class="page-item">
+            <a class="page-link" href="#">3</a>
+          </li>
+
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next"> &raquo; </a>
+          </li> -->
+        </ul>
+      </nav>
     </div>
   </section>
 </template>
