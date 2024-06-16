1d8cb1f58cc92b2ee578c9305bb4329989b872ab
LeonardSuli <leonardsuli1991@gmail.com>
2024-06-16 12:32:42 +0200

    add modal when i click a photo

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,7 @@ export default {
 
     // When i click the button search
     search() {
-      const url =
-        this.base_api_url + this.photos_endpoint + `?search=${this.search_text}`;
+      const url = this.base_api_url + this.photos_endpoint + `?search=${this.search_text}`;
 
       console.log(url);
 
@@ -62,17 +61,19 @@ export default {
       <p class="col-md-8 fs-4">See our amazing album</p>
 
       <form @submit.prevent="search()">
-        <div class="input-group mb-3">
-          <input
-            type="search"
-            class="form-control"
-            placeholder="Search..."
-            v-model="search_text"
-          />
 
-          <button class="btn btn-outline-secondary" type="submit">Search</button>
+        <div class="input-group mb-3">
+
+          <input type="search" class="form-control" placeholder="Search..." v-model="search_text"/>
+
+          <button class="btn btn-outline-secondary" type="submit">
+            Search
+          </button>
+
         </div>
+
       </form>
+
     </div>
   </div>
 
@@ -80,62 +81,94 @@ export default {
     <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
         <div class="col" v-for="photo in photos.data">
-          <div class="card">
-            <div v-if="photo.upload_image">
-              <img
-                class="card-img-top"
-                :src="
-                  photo.upload_image.startsWith('https://')
-                    ? photo.upload_image
-                    : base_api_url + '/storage/' + photo.upload_image
-                "
-                alt=""
-              />
+
+          <!-- Modal trigger button -->
+          <a data-bs-toggle="modal" :data-bs-target="`#photo-${photo.id}`">
+
+            <div class="card pointer">
+
+              <div v-if="photo.upload_image">
+                <img class="card-img-top" :src="photo.upload_image.startsWith('https://') ? photo.upload_image : base_api_url + '/storage/' + photo.upload_image" alt=""/>
+              </div>
+
+              <div v-else>
+                <img width='100%' src="https://picsum.photos/400/200" alt="" />
+              </div>
+
+              <div class="card-body">
+                {{ photo.title }}
+              </div>
+
             </div>
 
-            <div v-else>
-              <img src="https://picsum.photos/400/200" alt="" />
-            </div>
+          </a>
 
-            <div class="card-body">
-              {{ photo.title }}
+          <!-- Modal Body -->
+          <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+          <div class="modal fade" :id="`photo-${photo.id}`" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"
+            role="dialog" :aria-labelledby="`modal-title-${photo.id}`" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl" role="document">
+              <div class="modal-content">
+
+                <div class="modal-header">
+
+                  <h5 class="modal-title" :id="`modal-title-${photo.id}`">
+                    {{ photo.title }}
+                  </h5>
+
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                </div>
+
+                <div class="modal-body d-flex ">
+
+                  <div class='col-8'>
+                    <div v-if="photo.upload_image">
+                      <img class="card-img-top" :src="photo.upload_image.startsWith('https://') ? photo.upload_image : base_api_url + '/storage/' + photo.upload_image" alt=""/>
+                    </div>
+
+                    <div v-else>
+                      <img width='100%' src="https://picsum.photos/400/200" alt="" />
+                    </div>
+                  </div>
+
+                  <div class='col-4 ps-3'>
+                  
+                    {{ photo.description }}
+                    
+                  </div>
+
+                </div>
+
+                <div class="modal-footer">
+
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    Close
+                  </button>
+
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
       </div>
 
+      <!-- Pagination -->
       <nav aria-label="Page navigation" class="mt-4">
         <ul class="pagination">
-          <li
-            class="page-item"
-            :class="{ disabled: !link.url, active: link.active }"
-            v-for="link in photos.links"
-          >
-            <button
-              class="page-link"
-              :href="link.url"
-              type="button"
-              @click="goTo(link.url)"
-            >
+
+          <li class="page-item" :class="{ disabled: !link.url, active: link.active }" v-for="link in photos.links">
+
+            <button class="page-link" :href="link.url" type="button" @click="goTo(link.url)">
               <span v-html="link.label"></span>
             </button>
+            
           </li>
 
-          <!-- <li class="page-item active" aria-current="page">
-            <a class="page-link" href="#">1</a>
-          </li>
-
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-
-          <li class="page-item">
-            <a class="page-link" href="#">3</a>
-          </li>
-
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next"> &raquo; </a>
-          </li> -->
         </ul>
       </nav>
+      
     </div>
   </section>
 </template>
